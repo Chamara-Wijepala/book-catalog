49175d8ef761f648803257b98f8fe38b1cd97e3c
Chamara-Wijepala <mr.worf777@gmail.com>
2024-09-25 15:54:09 +0530

    Allow user to select from a list of genres on genres page

--- controllers/genresController.js
+++ controllers/genresController.js
@@ -3,7 +3,11 @@ const queries = require('../db/queries');
 
 async function getAllGenres(req, res) {
 	const { rows } = await pool.query(queries.getAllGenres);
-	res.render('genres', { genres: rows });
+	// If req.query.genre doesn't exist, checkedGenre is set to undefined.
+	// It always needs to be an array.
+	const checkedGenres = req.query.genre ? req.query.genre : [];
+
+	res.render('genres', { genres: rows, checkedGenres });
 }
 
 module.exports = {
